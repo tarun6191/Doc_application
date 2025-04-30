84bf641531da17dc8927e86ab8a412e4b09790cd
tarun6191 <128794331+tarun6191@users.noreply.github.com>
2025-04-29 20:20:58 -0500

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
